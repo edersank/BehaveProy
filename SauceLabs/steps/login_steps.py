3bbf50e686937371db423d2e67db23f878b62c8b
Eder Morales <emoralesc@eglobal.com.mx>
2025-03-02 19:57:52 -0600

    [@emoralesc] Se finaliza el guión de los steps

--- SauceLabs/steps/login_steps.py
+++ SauceLabs/steps/login_steps.py
@@ -9,7 +9,7 @@ from models.login_page import LoginPage
 async def step_usuario_en_login(context):
     await context.page.goto("https://www.saucedemo.com/v1/index.html")
 
-    await context.page.pause()
+    # await context.page.pause()
 
     login = LoginPage(context.page)
 
@@ -24,11 +24,32 @@ async def step_ingresa_nombre_usuario(context, user):
     await usuario.fill_user(user)
 
 
+@when('deja el campo de usuario vacío')
+@async_run_until_complete
+async def step_ingresa_nombre_usuario_vacio(context):
+    usuario = LoginPage(context.page)
+    await usuario.fill_user("")
+
+
 @when('ingresa el password "{password}"')
 @async_run_until_complete
 async def step_ingresa_password(context,  password):
     user_password = LoginPage(context.page)
     await user_password.fill_password(password)
+
+
+@when('ingresa una contraseña incorrecta "{password}"')
+@async_run_until_complete
+async def step_ingresa_password_incorrecta(context,  password):
+    user_password = LoginPage(context.page)
+    await user_password.fill_password(password)
+
+
+@when('deja el campo de contraseña vacío')
+@async_run_until_complete
+async def step_ingresa_password_vacio(context):
+    user_password = LoginPage(context.page)
+    await user_password.fill_password("")
 
 
 @when('hace clic en el boton de Login')
@@ -43,3 +64,24 @@ async def step_click_boton_loging(context):
 async def step_usuario_redigido_pagina_principal(context, text):
     redirected = LoginPage(context.page)
     await redirected.check_text(text)
+
+
+@then('debería ver un mensaje de error "{text}"')
+@async_run_until_complete
+async def step_error_user_password_no_match(context, text):
+    error_msg = LoginPage(context.page)
+    await error_msg.check_error_text(text)
+
+
+@then('debería ver un mensaje de error de datos incompletos "{text}"')
+@async_run_until_complete
+async def step_error_datos_imcompletos(context, text):
+    error_msg = LoginPage(context.page)
+    await error_msg.check_error_text(text)
+
+
+@then('debería ver un mensaje de error de usuario bloqueado "{text}"')
+@async_run_until_complete
+async def step_error_locked_user(context, text):
+    error_msg = LoginPage(context.page)
+    await error_msg.check_error_text(text)
